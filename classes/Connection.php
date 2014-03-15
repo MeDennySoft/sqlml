96f0cb87230ed0ec1b5b53f32c43a43ee029cc15
Daniel Morales <medennysoft@outlook.com>
2014-03-14 23:30:37 -0600

    Has consultas a bases de datos utilizando métodos.
    SQL methods Layer(SQLML) es una colección de clases que nos permiten de
    manera fácil y sencilla realizar consultas a la base de datos mediante
    el uso de simples métodos PHP. Utilizando objetos de datos PHP o por sus
    siglas en ingles PDO (PHP DATA OBJECTS). De este modo podemos asegurar
    un acceso seguro.

---
+++ classes/Connection.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace sqllayer;
+
+class Connection {
+
+    private $host;
+    private $user;
+    private $password;
+    private $dataBase;
+    private $dbms = "mysql";
+    private $connection;
+    private $language = "DML";
+
+    /**
+     * ConstructorClass
+     *
+     * Initialize the members when a new object is created
+     *
+     * @param expects an array with the information to connect using PDO
+     *        array['host']
+     *        array['user']
+     *        array['password']
+     *        array['database']
+     *        array['dbms'] 
+     *        array['lang'] = DDL | DML
+     */
+    public function __construct(array $tCfg) {
+        if (!is_array($tCfg)) {
+            return false;
+        }
+
+        if (key_exists("dbms", $tCfg)) {
+            $this->dbms = $tCfg['dbms'];
+        }
+
+        if (!key_exists("database", $tCfg)) {
+            $this->language = "DDL";
+        } else {
+            $this->dataBase = $tCfg['database'];
+        }
+
+        try {
+            $this->host = $tCfg['host'];
+            $this->user = $tCfg['user'];
+            $this->password = $tCfg['password'];
+            unset($tCfg);
+            $this->createConnection();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    private function createConnection() {
+        $connectionString = $this->makeConString();
+        try {
+            if ($this->dbms == "sqlite") {
+                $this->connection = new \PDO("sqlite:$this->host");
+            } else {
+                $this->connection = new \PDO("$connectionString", "$this->user", "$this->password");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    private function makeConString() {
+        $str = "$this->dbms:host=$this->host";
+        if ($this->language === "DML") {
+            $str += ";dbname=$this->dataBase";
+        }
+        return $str;
+    }
+
+    public function getInstance() {
+        return $this->connection;
+    }
+
+}
+?>
---
+++ classes/SqlCollection.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace sqllayer;
+
+class SqlCollection {
+
+    private $handler;
+    private $tConfig;
+    private $sqlTable;
+    private $placeHolders;
+    private $columns;
+    private $arrayExecutor = Null;
+    private $arrayCond = array();
+
+    public function __construct(array $config = null) {
+        $this->setStr($config);
+        $this->init();
+    }
+
+    public function setStr(array $config) {
+        $this->handler = new \sqllayer\Connection($config);
+        $this->tConfig = $config;
+        $this->handler = $this->handler->getInstance();
+    }
+
+    private function init() {
+        $this->sqlTable['get_all_regs'] = "SELECT <%col%> FROM <%table%> <%cond%>";
+        $this->sqlTable['set_reg'] = "INSERT INTO <%table%>(<%col%>) VALUES (<%vals%>) <%cond%>";
+        $this->sqlTable['update_reg'] = "UPDATE <%table%> SET <%col%> <%cond%>";
+        $this->sqlTable['delete_reg'] = "DELETE <%col%> FROM <%table%> <%cond%>";
+    }
+    
+    // DML
+    private function queryExecutor($stat, $arg) {
+        #$this->handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if (!$arg['table']) {
+            if (key_exists("table_default", $this->tConfig)) {
+                $arg['table'] = $this->tConfig['table_default'];
+            } else {
+                return false;
+            }
+        }
+
+        if ($arg['cond']) {
+            $arg['cond'] = "WHERE " . $arg['cond'];
+        }
+
+        // getting the sql statement
+        $statement = $this->sqlTable["$stat"];
+
+        // doing a well formed statement
+        foreach ($arg as $clave => $valor) {
+            $statement = str_replace('<%' . $clave . '%>', $valor, $statement);
+        }
+
+        // "Statement Handle"
+        if (!($STH = $this->handler->prepare("$statement"))) {
+            return False;
+        }
+
+        // placeholders well formed
+        if (is_array($this->arrayExecutor)) {
+            $this->arrayExecutor = array_merge($this->arrayExecutor, $this->arrayCond);
+        } else {
+            $this->arrayExecutor = $this->arrayCond;
+        }
+
+        if (!($STH->execute($this->arrayExecutor))) {
+            return False;
+        }
+
+        return $STH;
+    }
+
+    private function prepareUpdate() {
+        if (substr_count($this->columns, ",") == 0) {
+            $this->columns = "$this->columns = $this->placeHolders";
+            return true;
+        }
+
+        $columns = explode(",", $this->columns);
+        $this->columns = "";
+
+        foreach ($columns as $value) {
+            $this->columns .= " $value = :$value,";
+        }
+
+        $this->columns = rtrim($this->columns, ",");
+        return true;
+    }
+
+    //
+    public function set_col($cols, $vals) {
+        // strip some spaces
+        $cols = str_replace(" ", "", $cols);
+        $vals = str_replace(" ", "", $vals);
+
+        // creating the start up array
+        $columns = explode(",", $cols);
+        $colsVals = explode(",", $vals);
+
+        if (substr_count($cols, ",") == 0) {
+            unset($this->arrayExecutor);
+            $this->placeHolders = ":$cols";
+            $this->columns = "$cols";
+            $this->arrayExecutor[":$cols"] = "$vals";
+            return true;
+        }
+
+        // adding : to the cols
+        $stringFormed = "";
+        $stringCols = "";
+        foreach ($columns as $value) {
+            $value = trim($value);
+            $stringCols .= "$value,";
+            $stringFormed .= ":$value,";
+        }
+
+        // reconstructing the string
+        $this->placeHolders = rtrim($stringFormed, ",");
+        $this->columns = rtrim($stringCols, ",");
+
+        // making the array for executing the pdo query 
+        $columns = explode(",", $this->placeHolders);
+        $i = 0;
+        foreach ($columns as $col) {
+            $col = trim($col);
+            $this->arrayExecutor["$col"] = "$colsVals[$i]";
+            $i += 1;
+        }
+    }
+
+    public function set_cond($arr, $var = false, $val = false) {
+        if (!$arr) {
+            $this->arrayCond[":$var"] = "$val";
+        } else {
+            $this->arrayCond = $var;
+        }
+    }
+
+    //
+    public function get_reg($col, $cond = false, $table = false) {
+        $arg = array('col' => $col, 'cond' => $cond, 'table' => $table);
+        $type = "get_all_regs";
+        $STH = $this->queryExecutor($type, $arg);
+        unset($arg);
+        return $STH;
+    }
+
+    public function set_reg($cond = false, $table = false) {
+        $arg = array('col' => $this->columns, 'vals' => $this->placeHolders, 'cond' => $cond, 'table' => $table);
+        $type = "set_reg";
+        $r = $this->queryExecutor($type, $arg);
+        unset($arg);
+        return $r;
+    }
+
+    public function update_reg($cond = false, $table = false) {
+        $this->prepareUpdate();
+        $arg = array('col' => $this->columns, 'cond' => $cond, 'table' => $table);
+        $type = "update_reg";
+        $r = $this->queryExecutor($type, $arg);
+        unset($arg);
+        return $r;
+    }
+
+    public function delete_reg($col = Null, $cond = null, $table = false) {
+        $arg = array('col' => $col, 'cond' => $cond, 'table' => $table);
+        $type = "delete_reg";
+        $r = $this->queryExecutor($type, $arg);
+        unset($arg);
+        return $r;
+    }
+    
+    
+    public function shutdown_Connection() {
+        $this->handler = Null;
+    }
+
+    public function get_handler() {
+        return $this->handler;
+    }
+
+}
+
+?>
